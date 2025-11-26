907d1e9ac3dd6a758c76e55b9d02f8725ca6b2b1
Yasser Talbi <89804648+Abdelrahman-Yasser-TALBI@users.noreply.github.com>
2025-11-26 23:35:23 +0100

    Add statistiques.h header file with function prototypes

---
+++ statistiques.h
@@ -0,0 +1,14 @@
+#ifndef STATISTIQUES_H
+#define STATISTIQUES_H
+
+#define FICHIER_DONNEES "parking.txt"
+
+// Prototypes des fonctions - Étudiant 3
+void afficherStatistiques();
+void sauvegarderDonnees();
+void chargerDonnees();
+float calculerRevenuTotal();
+float calculerDureeMoyenne();
+int compterVoituresSorties();
+
+#endif
